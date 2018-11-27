39f6b27a9babbe3dd56b856f99794efce4aca61c
Etsu Kanegae <etsugetover0514@gmail.com>
2018-11-27 21:25:29 +0900

    @Timedアノテーションを付与

--- src/main/java/com/example/ek/prometheusdemo/web/controller/HelloContoller.java
+++ src/main/java/com/example/ek/prometheusdemo/web/controller/HelloContoller.java
@@ -3,12 +3,15 @@ package com.example.ek.prometheusdemo.web.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.micrometer.core.annotation.Timed;
+
 /**
  *
  * @author etsukanegae
  *
  */
 @RestController
+@Timed
 public class HelloContoller {
 
 	@GetMapping("/hello")
